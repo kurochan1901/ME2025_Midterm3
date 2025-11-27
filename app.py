ad27d288663a1983bd24ac58b16be590f12583cc
kurochan1901 <superstickman19.mc@gmail.com>
2025-11-27 14:43:04 +0800

    Update: form-product category. -Tested

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from core.database.database import Database
+from flask import Flask, render_template
 
 app = Flask(__name__)
-db = Database()
 
 @app.route('/', methods=['GET'])
 def index():
-    if request.method == 'GET':
-        orders = db.get_all_orders()
-        if request.args.get('warning'):
-            warning = request.args.get('warning')
-            return render_template('form.html', orders=orders, warning=warning)
-        return render_template('form.html', orders=orders)
-
+    # 假資料（前端測試用）
+    orders = [
+        {"id": 1, "product": "T-Shirt", "qty": 2, "subtotal": 50},
+        {"id": 2, "product": "Shoes", "qty": 1, "subtotal": 90},
+    ]
+    return render_template('form.html', orders=orders)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5500, debug=True)
